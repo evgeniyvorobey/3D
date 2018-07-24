4ef3d7d571ba5705174bf8649c70a3f3be5845cc
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2018-07-24 15:31:14 +0300

    slider test

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,3 +31,30 @@ window.onload = function() {
         }
     }
 }
+
+var owl = $('.owl-slider-first');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
