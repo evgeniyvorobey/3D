cab1c64aaaca01b77dfb71e0370edc004ca9407e
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2018-07-25 23:59:27 +0300

    last 25.07

--- src/js/script.js
+++ src/js/script.js
@@ -39,6 +39,7 @@ owl.owlCarousel({
     margin:10,
     autoplay:true,
     nav:true,
+    dotsEach:3,
     navContainer:'.nav-for-owl',
     dotsContainer:'.dot',
     navText : ["",""],
@@ -48,10 +49,10 @@ owl.owlCarousel({
         0:{
             items:2
         },
-        600:{
+        768:{
             items:3
         },
-        1000:{
+        1024:{
             items:4
         }
     }
@@ -62,3 +63,85 @@ $('.play').on('click',function(){
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
 })
+var owl = $('.owl-slider-second');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:true,
+    navContainer:'.nav-for-owl-second',
+    dotsContainer:'.dot-second',
+    navText : ["",""],
+    dotsEach:3,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        1024:{
+            items:3
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+var owl = $('.owl-slider-third');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:true,
+    dotsEach:3,
+    navContainer:'.nav-for-owl-third',
+    dotsContainer:'.dot-third',
+    navText : ["",""],
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        1024:{
+            items:3
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+
+var descriptionToggle = document.getElementsByClassName('added-description-toggle')[0];
+
+descriptionToggle.onclick = function(event) {
+    var target = event.target;
+    for ( let i = 0 ; i < this.childElementCount ; i++) {
+        this.children[i].classList.remove('active-btn');
+    };
+    target.classList.add('active-btn');
+    console.log(target.tagName)
+    var block = target.parentNode.parentNode.parentNode.children[1].children;
+    for ( let i = 0; i < block.length; i++ ) {
+        block[i].classList.remove('content-active');
+    }
+    if ( target.id == 'btn-first') {
+        block[0].classList.add('content-active');
+    } else if ( target.id == 'btn-second') {
+        block[1].classList.add('content-active');
+    } else if ( target.id == 'btn-third') {
+        block[2].classList.add('content-active');
+    }
+}
