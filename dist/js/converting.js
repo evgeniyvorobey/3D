c3be119372945b8be47abc4fc32373e983a0dc08
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2018-08-09 17:34:00 +0300

    support all

--- dist/js/converting.js
+++ dist/js/converting.js
@@ -5,11 +5,12 @@ var plus = document.getElementsByClassName('plus')[0];
 var windowIco = document.getElementsByClassName('window')[0];
 var addDropedFile = document.getElementById('add-droped-file');
 var purchasedBlock = document.getElementsByClassName('from-purchased-block')[0];
+var priceTitle = document.getElementById('price-request-title');
+var requestButtonPosition = document.getElementsByClassName('price-request')[0];
+var fromPurchasedButton = document.getElementById('from-purchased-button');
+var closePurchasedBlock = document.getElementById('close-purchased-block');
+var blockWithPurchasedItem = document.getElementsByClassName('purchased-item')[0];
 
-function toggleBlock() {
-    addDropedFile.classList.toggle('visible');
-    purchasedBlock.classList.toggle('visible');
-}
 
 addProduct.onclick = function() {
     remooveActiveClass();
@@ -17,7 +18,12 @@ addProduct.onclick = function() {
     for ( var i = 0; i < plus.children.length; i++ ) {
         plus.children[i].classList.add('plus-active');
     }
-    toggleBlock();
+    priceTitle.style.display = 'flex';
+    requestButtonPosition.style.justifyContent = 'space-between';
+    
+    addDropedFile.classList.add('visible');
+    purchasedBlock.classList.remove('visible');
+
 }
 
 fromPurchased.onclick = function() {
@@ -26,7 +32,13 @@ fromPurchased.onclick = function() {
     for ( var i = 0; i < plus.children.length; i++ ) {
         windowIco.children[i].classList.add('window-active');
     }
-    toggleBlock();
+    priceTitle.style.display = 'none';
+    requestButtonPosition.style.justifyContent = 'flex-end';
+
+    addDropedFile.classList.remove('visible');
+    purchasedBlock.classList.add('visible');
+
+
 }
 
 function remooveActiveClass() {
@@ -42,11 +54,111 @@ function remooveActiveClass() {
 
 
 
+fromPurchasedButton.onclick = function() {
+    shadeOn();
+    blockWithPurchasedItem.classList.add('purchased-item-active');
+}
+
+closePurchasedBlock.onclick = function() {
+    shadeOff();
+    blockWithPurchasedItem.classList.remove('purchased-item-active');
+}
+
+// mouse event
+
+// var purchasedItemBlock = document.getElementsByClassName('purchased-item-block')[0];
+
+// var currentElement = null;
+
+// purchasedItemBlock.onmouseover = function(e) {
+//     if (currentElement) {
+//         return;
+//     }
+//     target = e.target;
+// console.log(this)
+//     while ( target != this) {
+//         if (target.id == 'mouse-on' )break;
+//             target = target.parrentNode;
+//     }
+//     if ( target == this ) return;
+    
+//     currentElement = target;
+//     target.classList.add('active-mouse-on');
+//     target.children[0].classList.add('add-to-convertation')
+// };
+
+// purchasedItemBlock.onmouseout = function(e) {
+//     if (!currentElement) return;
+
+//     var relatedTarget = event.relatedTarget;
+//     if (relatedTarget) {
+//         while (relatedTarget) {
+//             if (relatedTarget == currentElement) return;
+//             relatedTarget = relatedTarget.parrentNode;
+//         }
+//     }
+
+//     currentElement.classList.remove('active-mouse-on');
+//     currentElement.children[0].classList.remove('add-to-convertation')
+//     currentElement = null;
+// }
+
+var purchasedItemBlock = document.getElementsByClassName('purchased-item-block')[0];
+
+
+var addToConvertation = document.getElementById('add-to-convertation');
+var deleteButton = document.getElementById('delete-from-convertation');
+
+
+purchasedItemBlock.onclick = function(e) {
+    var addMyElemrntToConverting = document.getElementById('add-my-elemrnt-to-converting');
+    var target = e.target; 
+    if ( target.id == 'add-to-convertation' ){
+        var elSrc = target.parentNode.parentNode.children[0].currentSrc;
+        target.classList.remove('active-button-for-select-item');
+        target.nextElementSibling.classList.add('active-button-for-select-item');
+        target.parentNode.nextElementSibling.classList.add('active-button-for-select-item');
+        target.parentNode.parentNode.parentNode.id = 'add';
+        purchasedBlock.innerHTML += `<div class="preconvering-item">
+                                        <img src="${elSrc}" alt="">
+                                    </div>`
+    } else if ( target.id == 'delete-from-convertation' ) {
+        target.classList.remove('active-button-for-select-item');
+        target.previousElementSibling.classList.add('active-button-for-select-item');
+        target.parentNode.nextElementSibling.classList.remove('active-button-for-select-item');
+        target.parentNode.parentNode.parentNode.id = '';
+    } else {
+        return;
+    }
+    for( var i = 0; i < purchasedItemBlock.children.length; i++) {
+        if (purchasedItemBlock.children[i].id == 'add') {
+            addMyElemrntToConverting.classList.add('active-button-for-select-item');
+            return;
+        } else {
+            addMyElemrntToConverting.classList.remove('active-button-for-select-item');
+            return;
+        }
+    }
+}
+
+
+
+
+addToConvertation.onclick = function() {
+    addToConvertation.classList.remove('active-button-for-select-item');
+    deleteButton.classList.add('active-button-for-select-item');
+}
+
+
+
+
+
+
+
+
+
+
 var dropZone = document.getElementById('drop-zone');
-// dropZone.addEventListener('dragenter', handlerFunction, false);
-// dropZone.addEventListener('dragleave', handlerFunction, false);
-// dropZone.addEventListener('dragover', handlerFunction, false);
-// dropZone.addEventListener('drop', handlerFunction, false);
 
 // Обработчики событий над областью drag
 ['dragenter','dragover','dragleave','drop'].forEach(eventName => {
@@ -85,6 +197,8 @@ function handleFiles(files) {
     ([...files]).forEach(uploadFile)
 }
 
+
+// ---------------отправка на сервер
 function uploadFile(file) {
     var url = 'https://api.cloudinary.com/v1_1/YOU/image/upload';
     var formData = new FormData();
@@ -102,7 +216,7 @@ function uploadFile(file) {
         console.log('fail')
     })
 }
-
+// ---------------рендер на странице
 function previewFile(file) {
     let reader = new FileReader()
     reader.readAsDataURL(file)
@@ -115,10 +229,7 @@ function previewFile(file) {
 
 function handleFiles(files) {
     files = [...files]
-    files.forEach(uploadFile)
+    // files.forEach(uploadFile)
     files.forEach(previewFile)
-    arr += files.map(function(item, i, filter){
-        arr[item]
-        // console.log(arr)
-    })
+
 }
